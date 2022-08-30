cb230d6786f2284fc6d69ea4ccb86765a205fcea
SapientCoffee <rob.j.edwards@gmail.com>
2022-08-30 20:01:02 +0000

    troubleshoot

--- app/app.py
+++ app/app.py
@@ -4,20 +4,25 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template("index.html")
+def hello_world():
+    name = os.environ.get("NAME", "World")
+    return "Hello {}!".format(name)
 
-@app.route("/about")
-def about():    
-    return render_template("about.html")
+# @app.route("/")
+# def home():
+#     return render_template("index.html")
 
-@app.route("/404")
-def notfound():    
-    return render_template("404.html")
+# @app.route("/about")
+# def about():    
+#     return render_template("about.html")
+
+# @app.route("/404")
+# def notfound():    
+#     return render_template("404.html")
     
-@app.route("/coffeesay")
-def salvador():
-    return "The best `cowsay` fork ever :-)"
+# @app.route("/coffeesay")
+# def salvador():
+#     return "The best `cowsay` fork ever :-)"
     
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
