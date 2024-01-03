8ddd3ee3d226f729305da5552292fedc4d1e90bf
SapientCoffee <rob.j.edwards@gmail.com>
2024-01-03 13:36:39 +0000

    env var passed to web app

--- app/app.py
+++ app/app.py
@@ -3,28 +3,12 @@ import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
-# os.getenv('name_of_variable')
-# os.environ
-# os.environ['name_of_variable']
-
-# @app.route("/")
-# def hello_world():
-#     name = os.environ.get("NAME", "World")
-#     return "Hello {}!".format(name)
 
 @app.route("/")
 def home():
-
-
-    # Iterate loop to read and print all environment variables
-    print("The keys and values of all environment variables:")
-    for key in os.environ:
-        print(key, '=>', os.environ[key])
-
-    # Print the value of the particular environment variable
-    print("The value of HOME is: ", os.environ['HOME'])
     
-    var = 'chemex'
+    var = os.environ['stage']
+    # var = 'chemex'
 
     return render_template("index.html", value=var)
 
@@ -36,9 +20,9 @@ def about():
 def notfound():    
     return render_template("404.html")
     
-@app.route("/coffeesay")
-def salvador():
-    return "The best `cowsay` fork ever :-)"
+# @app.route("/coffeesay")
+# def salvador():
+#     return "The best `cowsay` fork ever :-)"
     
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
